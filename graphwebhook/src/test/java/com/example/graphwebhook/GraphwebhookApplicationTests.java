41747c789ee9d36bde208b583dea9e092a009619
Jason Johnston <jasonjoh@microsoft.com>
2021-10-04 17:05:08 -0400

    Update GraphwebhookApplicationTests.java

--- graphwebhook/src/test/java/com/example/graphwebhook/GraphwebhookApplicationTests.java
+++ graphwebhook/src/test/java/com/example/graphwebhook/GraphwebhookApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.graphwebhook;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SuppressWarnings("java:S2187")
 class GraphwebhookApplicationTests {
 
 }
